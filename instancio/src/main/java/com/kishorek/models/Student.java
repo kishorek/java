2c4915076ca69d74c12d90ae37877bc8bcceb991
kishorek <libcomet@gmail.com>
2023-01-21 18:26:28 -0500

    Sample updated

--- instancio/src/main/java/com/kishorek/models/Student.java
+++ instancio/src/main/java/com/kishorek/models/Student.java
@@ -9,9 +9,9 @@ import java.util.Date;
 @ToString
 public class Student {
     private String name;
-    private String id;
-    private char section;
-    private double cgpa;
-    private String house;
-    private Date dateOfJoining;
+    private String id; //ID Format: LSV<ID>
+    private char section; //The student can be placed in any sections from 'A' - 'K'
+    private double cgpa; //Can be between 0.0 and 4.0
+    private String house; // The school has 'White', 'Blue', 'Red', 'Green' sport houses
+    private Date dateOfJoining; // Should be a past date
 }
--- instancio/src/test/java/Tester.java
+++ instancio/src/test/java/Tester.java
@@ -11,20 +11,25 @@ import java.util.Map;
 public class Tester {
     @Test
     public void testStudent() {
-        Student sample = Instancio.create(Student.class);
-        System.out.println(sample);
+        //Creating an object using Shorthand method
+        Student student = Instancio.create(Student.class);
+        System.out.println(student);
 
-        Map<String, Long> map = Instancio.create(new TypeToken<Map<String, Long>>() {
-        });
-        System.out.println(map);
+        //Creating the object using builder
+        student = Instancio.of(Student.class).create();
+        System.out.println(student);
 
-        System.out.println(Instancio.ofMap(String.class, Long.class).size(20).create());
+        // Setting the name field to Carl
+        Student namedStudent = Instancio.of(Student.class)
+                .set(Select.field(Student::getName), "Carl").create();
+        System.out.println(namedStudent);
 
-        Model<Student> studentModel = Instancio.of(Student.class).toModel();
-        System.out.println(Instancio.of(studentModel).create());
+        // Setting all the String fields to some value
+        Student allSelectStudent = Instancio.of(Student.class).set(Select.all(String.class),"Some String").create();
+        System.out.println(allSelectStudent);
 
         Student selectorSample = Instancio.of(Student.class)
-                .set(Select.field(Student::getName), "NewStudent")
+                .set(Select.field(Student::getName), "Carl")
                 .generate(Select.field(Student::getCgpa), gen -> gen.doubles().range(0.0, 10.0))
                 .generate(Select.field(Student::getId),gen->gen.text().pattern("LSV#d"))
                 .generate(Select.field(Student::getHouse),gen->gen.oneOf("White","Blue","Red","Green"))
@@ -32,5 +37,11 @@ public class Tester {
                 .create();
         System.out.println(selectorSample);
 
+        //Creating Maps
+        Map<String, Long> map = Instancio.create(new TypeToken<Map<String, Long>>() {
+        });
+        System.out.println(map);
+
+        System.out.println(Instancio.ofMap(String.class, Long.class).size(20).create());
     }
 }
